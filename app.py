c52830cfc2de7c17a22f52608e690e196c442b24
Prathamesh Bandewar <prathmeshbandewar@gmail.com>
2026-06-26 18:43:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import nltk
+import os
+
+nltk_data_path = "/tmp/nltk_data"
+
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+nltk.download("stopwords", download_dir=nltk_data_path)
+
 from flask import Flask, render_template, request
 import joblib
 import re
